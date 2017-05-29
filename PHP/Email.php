75028504a4d6b64f7dac99762f1ada571ea8ed24
Aaron-RN <Aaron_Newbold@hotmail.com>
2017-05-28 21:13:47 -0400

    Av

--- PHP/Email.php
+++ PHP/Email.php
@@ -1,16 +1,18 @@
 <?php
-    if(isset($_POST['submit'])) {
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
         $to = "aaron_newbold@hotmail.com";
         $subject = "Form Avenii.com";
         $name_field = $_POST['name'];
-        $number_field = $_POST['name'];
+        $number_field = $_POST['number'];
         $email_field = $_POST['email'];
         $message = $_POST['message'];
+        $message = wordwrap($message,70);
+        $headers = "From: webmaster@example.com";
 
         $body = "From: $name_field\n Number: $number_field\n E-Mail: $email_field\n Message:\n $message";
 
         echo "Data has been submitted to $to!";
-        mail($to, $subject, $body);
+        mail($to, $subject, $body, $headers);
         } else {
         echo "blarg!";
     }
